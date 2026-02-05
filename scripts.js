01ca0f67183a4465a8bf57b5df014e65def3a6e3
Steve Loriat <stevenloriat@gmail.com>
2026-02-05 10:33:31 +0100

    version -4

--- scripts.js
+++ scripts.js
@@ -5,3 +5,12 @@ test();
 function add(a, b) {
   return a + b;
 }
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error("Division by zero is not allowed.");
+  }
+  return a / b;
+}
